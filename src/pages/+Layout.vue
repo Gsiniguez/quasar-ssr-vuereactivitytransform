9286110a91da8f0595dde8e525d39acbc5cffa2e
Gonzalo Iñiguez <31194342+Gsiniguez@users.noreply.github.com>
2022-09-08 18:05:57 -0300

    first commit

---
+++ src/pages/+Layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="hHh LpR fff">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Alimenta Tu Salud </q-toolbar-title>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+          class="rmob"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
---
+++ src/pages/+Page.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page>
+    <div class="rmob">HOME PAGE MOBILE</div>
+    <div class="rtab">HOME PAGE TABLET</div>
+    <div class="rdes">HOME PAGE DESKTOP</div>
+    <q-btn label="Chnage" @click="changename" />
+    {{ name }}
+  </q-page>
+</template>
+
+<script setup>
+let name = $ref('test');
+
+const changename = () => {
+  name = 'changed';
+};
+</script>
+<style lang=""></style>
